989eed8c47990635e1a9a02bb68c027a8975d5d5
Sourajit <sourajitmandal19@gmail.com>
2024-01-09 23:18:31 +0530

    backend code ... database try

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+// backend/server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const apiRoutes = require('./routes/api'); 
+
+// Set proxy environment variables
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Connect to MongoDB (replace 'your-mongodb-uri' with your actual MongoDB URI)
+mongoose.connect('mongodb+srv://Sourajit:7811069775@clusterwebster.ugk1voi.mongodb.net/Students', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+
+
+
+// Define routes (you'll create these later)
+// app.use('/api', require('./routes/api'));
+app.use('/api', apiRoutes);
+
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

8e68982c2e126a886175f8618d506f9691f70232
Sourajit Mandal <140224488+souraman19@users.noreply.github.com>
2024-08-13 21:28:25 +0530

    change style of gallery segment of main page

--- frontend/src/components/commonComponents/ControlledCarousel.jsx
+++ frontend/src/components/commonComponents/ControlledCarousel.jsx
@@ -29,38 +29,25 @@ function ControlledCarousel() {
           activeIndex={index}
           onSelect={handleSelect}
           className="carousel-messmenu-cc"
+          style={{ width: "90vw", height: "80vh", margin: "auto" , marginBottom: "110px"}}
         >
           <Carousel.Item>
             <ExampleCarouselImage
               text="First slide"
               mysrc="./images/food1.jpg"
             />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
             <ExampleCarouselImage
               text="Second slide"
               mysrc="./images/food2.jpg"
             />
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
             <ExampleCarouselImage
               text="Third slide"
               mysrc="./images/food1.jpg"
             />
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
       </div>

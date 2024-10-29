2a07b27866972e096a3ebd85c763e4bc890ec3bb
Sourajit Mandal <140224488+souraman19@users.noreply.github.com>
2024-10-29 15:32:04 +0530

    Add rating model for each meal and each meal item

---
+++ backend/models2.0/mess_menu/MessMenuPerSlotRatingBucket.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+import {User} from "../person/User.js";
+import MessMenuPerSlot from "./../mess_menu/MessMenuPerSlot.js";
+
+const ratingSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User', 
+    },
+    rating: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5, 
+    },
+    comment: {
+        type: String,
+        default: null, 
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const messMenuPerSlotRatingBucketSchema = new mongoose.Schema({
+    mealId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'MessMenuPerSlot', 
+    },
+    bucketId: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    ratings:{
+        type:[ratingSchema],
+        default:[]
+    },
+    ratingsCount:{
+        type: Number,
+        default: 0
+    },
+}, {timestamps: true});
+const MessMenuPerSlotRatingBucket = new mongoose.model('MessMenuPerSlotRatingBucket', messMenuPerSlotRatingBucketSchema);
+export default MessMenuPerSlotRatingBucket;  
+
+
+

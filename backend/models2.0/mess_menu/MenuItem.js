d87b9d50707006d946be1844e5d56248128fe638
Sourajit Mandal <140224488+souraman19@users.noreply.github.com>
2024-10-29 13:34:18 +0530

    update MenuItem model

--- backend/models2.0/mess_menu/MenuItem.js
+++ backend/models2.0/mess_menu/MenuItem.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
+import {MessWorker, Cook} from "./../person/MessWorker";
 
 const menuItemSchema = new mongoose.Schema({
     title: {
         type: String,
         required: true,
-        unique: true
+        unique: true ,//no to menu Items can have same name
     },
     image: {
         type: String, 
@@ -18,8 +19,19 @@ const menuItemSchema = new mongoose.Schema({
         amount:{type: Number, required: true},
         unit:{type: String, required: true},
     },
+    cookedBy: {
+        type:[{
+            cookName: {type: String, required: true},
+            cookId: {
+                type: mongoose.Schema.Types.ObjectId,
+                required: true,
+                ref: 'Cook'
+            }
+        }],
+        default:[]
+    },
 
-})
+}, { timestamps: true })
 
 const MenuItem = new mongoose.model('MenuItem', menuItemSchema);
 export default MenuItem;

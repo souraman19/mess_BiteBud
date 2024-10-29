7b6bc75e74f53d07822152d442de7a0450694a2c
Sourajit Mandal <140224488+souraman19@users.noreply.github.com>
2024-10-29 13:11:47 +0530

    GalleryItem model add

---
+++ backend/models2.0/gallery_item/GalleryItem.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+import {Student} from "./../person/User.js";
+
+const reactionUnderImageSchema = mongoose.Schema({
+    reaction: {type: String, required: true},
+    reactionTime: {type: Date, default: Date.now},
+    reactedBy: {
+        name:{type: String, required: true},
+        userId: {type: mongoose.Schema.Types.ObjectId, ref: 'Student', required: true}
+    },
+    isDeleted: { type: Boolean, default: false },
+}, {timestamps: {createdAt: true, updatedAt: true}});
+
+const galleryItemSchema = new mongoose.Schema({
+    itemId:{
+        type: mongoose.Schema.Types.ObjectId,
+        default: () => new mongoose.Types.ObjectId(),
+        unique: true,
+    },
+    uploadedBy: {
+        name: {type: String, required: true},
+        userId: {type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Student'}
+    },
+    uploadTime: {
+        type: Date,
+        default: Date.now,
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        default: ""
+    },
+    ractions:{
+        type:[reactionUnderImageSchema],
+        default: []
+    },
+    reactionCount: {
+        type: Number,
+        default: 0
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false,
+    }
+
+}, {timestamps: {createdAt: true, updatedAt: true}});
+
+const GalleryItem = new mongoose.model('GalleryItem', galleryItemSchema);
+
+export default GalleryItem;

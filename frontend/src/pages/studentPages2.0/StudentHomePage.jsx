10ddeb2f53af297a78872d4602bfcc5d86e2e05c
Sourajit Mandal <140224488+souraman19@users.noreply.github.com>
2024-12-27 20:26:23 +0530

    rename Patelintro

--- frontend/src/pages/studentPages2.0/StudentHomePage.jsx
+++ frontend/src/pages/studentPages2.0/StudentHomePage.jsx
@@ -4,7 +4,7 @@ import Gallary from "../../components/commonComponents/Gallery.jsx";
 import CurrentMessMenu from "../../components/commonComponents/CurrentMessMenu.jsx";
 // import Review from "../../components/commonComponents/Review.jsx";
 import CommentSegment from "../../components/commonComponents/CommentSegment.jsx";
-import Patelintro from "../../components/studentComponents/Patelintro.jsx";
+import HostelIntro from "../../components/studentComponents/HostelIntro.jsx";
 import Complaint from "../../components/commonComponents/Complaint.jsx";
 // import Randomcomplaint from "../../components/commonComponents/RandomComplaint.jsx";
 import ExpenseSegment from '../../components/commonComponents/ExpenseSegment.jsx';
@@ -18,7 +18,7 @@ function Patelstudent(){
     return(
         <div>
         <Navbar />
-         <Patelintro />
+         <HostelIntro />
         {/* <ExpenseSegment /> */}
 
         <CurrentMessMenu />  
